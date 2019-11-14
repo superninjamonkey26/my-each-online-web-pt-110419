a4f9af60b77dec33e6893e3b6227f93fabfaef7f
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-14 02:26:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,3 @@
-#def my_each(array) # put argument(s) here
-#array = []
-#while array == words.length
- # puts my_each(array)
-  # code here
-#end
-
 def my_each(array)
   i = 0
   while i < array.length
